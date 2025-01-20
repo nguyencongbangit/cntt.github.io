5e9044762884b9ad7b3d40fdbecd8d5514362ce9
thaygiaoit2025 <134355153+nguyencongbangit@users.noreply.github.com>
2025-01-20 08:21:28 +0700

    Add files via upload

---
+++ submit_form.php
@@ -0,0 +1,20 @@
+﻿<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Cấu hình email
+    $to = "congbang3110hp@gmail.com";
+    $subject = "Thông Tin Đăng Ký Mới";
+    $body = "Họ và Tên: $name\nEmail: $email\nSố Điện Thoại: $phone\nNội Dung:\n$message";
+
+    // Gửi email
+    if (mail($to, $subject, $body)) {
+        echo json_encode(["status" => "success"]);
+    } else {
+        echo json_encode(["status" => "error"]);
+    }
+}
+?>
